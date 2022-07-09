6d9bf2c0e1fb1c19ea4b35f7ddf01b2d56bbed1d
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-07-09 19:50:22 +0100

    info

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import {StyledHeader} from "./styles/Header.styled"
+const Header = () => {
+  return (
+    <StyledHeader><h1>Hubble</h1></StyledHeader>
+  )
+}
+
+export default Header
---
+++ src/components/styles/Container.styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+width:1000px;
+color:red;
+max-width: 0 20px;
+margin: 0 auto;
+
+
+`
